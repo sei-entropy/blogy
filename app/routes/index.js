3c0ba1450a0e90ff228badee42c623601dbf2d49
Usman Bashir <me@usmanbashir.com>
2020-03-08 11:12:29 +0300

    Add Index Route

---
+++ app/routes/index.js
@@ -0,0 +1,20 @@
+// Require necessary NPM packages
+const express = require('express');
+
+// Instantiate a Router (mini app that only handles routes)
+const router = express.Router();
+
+/**
+ * Action:      INDEX
+ * Method:      GET
+ * URI:         /
+ * Description: Get the Root Route
+ */
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Blogy'
+  });
+});
+
+// Export the Router so we can use it in the server.js file
+module.exports = router;
